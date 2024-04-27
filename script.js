7d9ee84f3cd17168eee13bf19bd4237da31dd396
strawberryaish <trismorgenstern@gmail.com>
2024-04-27 22:40:32 +0800

    updated validation

--- script.js
+++ script.js
@@ -13,74 +13,102 @@ const detailCardNo = document.querySelector('.card-no-detail'),
       detailYear = document.querySelector('.year-detail'),
       detailCVC = document.querySelector('.cvc-detail');
 
-//DEFAULT
-const defaultNo = '0000 0000 0000 0000', defaultDate = '00', defaultCVC = '000';
+//VALIDATION ID
+const validationNames = document.getElementById('validation-name'),
+      validationCardNo = document.getElementById('validation-cardno'),
+      validationDate = document.getElementById('validation-date'),
+      validationCVC = document.getElementById('validation-cvc');
 
 //Validations
 const checkValidations = {
-    emptyinput: function (inputName) { 
-        if (inputName.value.trim() === '') console.log('Cannot be empty'); 
-        else 
+    emptyinput: function (inputName, validationID) { 
+        if (inputName.value.trim() === '') {
+            validationID.textContent = "Can't be blank";
+       }
+        else {
+            validationID.innerHTML =  '&nbsp;';
             return true;
         
-        },
+        }},
 
-    checkLength: function(inputLength, maxLength) { 
+    checkLength: function(inputLength, maxLength, validationID) { 
         if (!inputLength.value) return;
-
-        if (inputLength.value.length !== maxLength) 
-            console.log('too short');
-          
-        else 
-           return true; 
+        
+        if (inputLength.value.length !== maxLength) {
+            validationID.textContent = 'Length is too short';
+        } 
+        else {
+           validationID.innerHTML = '&nbsp;';
+           return true; }
         },
 
-    checkCardNo: function(inputName, maxLength) { 
-        const nowhitespace = inputName.value.replaceAll(' ', '');
-        
-        const checkBoth = this.emptyinput(inputName) && this.checkLength(inputName, maxLength);
+    //FUNCTIONS FOR CHECKING INDIVIDUAL INPUT FIELDS
 
-        const checkFormat = isNaN(+nowhitespace) ? console.log('Only numeric input allowed') : true; 
-        
-        if (checkBoth && checkFormat) 
-            return true;
+    checkName: function(inputName, validationID) {
+        const checkEmpty = this.emptyinput(inputName, validationID);
 
-         },
+        inputName.className = checkEmpty ? 'success' : 'error';
 
-    checkYearMonth: function(expMonth, expYear, maxLength) {
+        return checkEmpty;
+    },
+
+    checkCardNo: function(inputName, maxLength, validationID) { 
+        const checkCardNoFormat = isNaN(+inputName.value.replaceAll(' ', ''));
+        const checkEmptyAndLength = this.emptyinput(inputName, validationID) && this.checkLength(inputName, maxLength, validationID);
+
+       checkCardNoFormat ? validationID.textContent = 'Wrong format, numbers only': true;
+
+        inputName.className = checkEmptyAndLength && !checkCardNoFormat ? 'success' : 'error';
+
+        return checkEmptyAndLength && !checkCardNoFormat;
+    },
+
+    checkYearMonth: function(expMonth, expYear, maxLength, validationID) {
         const month = expMonth.value, year = expYear.value;
-        let checkExpiration;
-
-        //Check length
-        if (month.value) this.checkLength(expMonth, maxLength);
-        if (year.value) this.checkLength(expYear, maxLength); 
         
         //Check format and expiration date
-        const checkBothLength = this.checkLength(expMonth, maxLength) && this.checkLength(expYear, maxLength);
-        const invalidMonth = expMonth.value > 12;
+        const checkMonthLength = this.checkLength(expMonth, maxLength, validationID);
+        const checkYearLength = this.checkLength(expYear, maxLength, validationID);
+        const invalidMonth = month > 12;
 
-        if (invalidMonth) console.log('Invalid month');
+        if (invalidMonth) {
+            validationID.textContent = 'Invalid Month';
+            expMonth.className = 'error'; }
 
-        if (checkBothLength && !invalidMonth) { 
+        else {
+            expMonth.className = ''; }
+
+        if (year){
+        expYear.className = checkYearLength ? '' : 'error'; }
+
+        if (month) {
+        expMonth.className = checkMonthLength ? '' : 'error'; }
+
+        if (checkMonthLength && checkYearLength && !invalidMonth ) { 
             const date = new Date(), 
                   currentYear = `${date.getFullYear()}`.slice(2),
                   currentMonth = `${date.getMonth() + 1}`.padStart(2, '0');
 
-        checkExpiration = currentYear > year || currentYear >= year && currentMonth > month ? console.log('expired') : true;   
-        
-        if (checkExpiration) 
-            return true;
-        
-        }
+            if (currentYear > year || currentYear >= year && currentMonth > month){
+                expMonth.className = 'error';
+                expYear.className = 'error';
+                validationID.textContent = 'Your card is already expired'; }
+            else {
+                expMonth.className = 'success';
+                expYear.className = 'success';
+                validationID.innerHTML = '&nbsp';
+                return true;
 
-        },
+        }}
+        
+    },
 
-    checkCVC: function(inputName, maxLength) {
-        const checkBoth =  this.emptyinput(inputName) &&  this.checkLength(inputName, maxLength);
+    checkCVC: function(inputName, maxLength, validationID) {
+        const checkBoth =  this.emptyinput(inputName, validationID) &&  this.checkLength(inputName, maxLength, validationID);
         
-        if (checkBoth) 
-            return true;
+        inputName.className = checkBoth ? 'success' : 'error';
         
+        return checkBoth;
     }
 
 }
@@ -95,8 +123,10 @@ const limitLength = (inputName, maxLength) => {
 //Event handlers
 //CARD HOLDER NAME
 inputName.addEventListener('input', function() {
-    limitLength(this, 25);
-    checkValidations.emptyinput(this);
+    const maxLength = 25;
+
+    limitLength(this, maxLength);
+    checkValidations.checkName(this, validationNames);
 
     !this.value ? detailName.textContent = 'Aisha Willows' : detailName.textContent = this.value;
   
@@ -121,13 +151,13 @@ inputCardNo.addEventListener('keydown', function(e) {
 
 
 inputCardNo.addEventListener('input', function() {
-    const maxLength = 19;
+    const maxLength = 19, defaultNo = '0000 0000 0000 0000';
     limitLength(this, maxLength);
 
     const updateCardNo = inputCardNo.value + defaultNo.slice(inputCardNo.value.length);
     detailCardNo.textContent = updateCardNo;
    
-    checkValidations.checkCardNo(this, maxLength);
+    checkValidations.checkCardNo(this, maxLength, validationCardNo);
 });
 
 //EXP DATE: MONTH
@@ -135,31 +165,32 @@ inputMonth.addEventListener('input', function() {
     const maxLength = 2;
     limitLength(this, maxLength);
 
-    const updateMonth = this.value.padStart(maxLength, defaultDate);
+    const updateMonth = this.value.padStart(maxLength, '0');
     detailMonth.textContent = updateMonth;
   
-    checkValidations.checkYearMonth(inputMonth, inputYear, maxLength);
+    checkValidations.checkYearMonth(inputMonth, inputYear, maxLength, validationDate);
     
 });
 
 //EXP DATE: YEAR
 inputYear.addEventListener('input', function() {
-    const maxLength = 2,
-          updateYear = this.value.padStart(maxLength, defaultDate);
+    const maxLength = 2;
     limitLength(this, maxLength);
 
+    const updateYear = this.value.padStart(maxLength, '0');
+
     detailYear.textContent = updateYear;
-    checkValidations.checkYearMonth(inputMonth, inputYear, maxLength);
+    checkValidations.checkYearMonth(inputMonth, inputYear, maxLength, validationDate);
     
 });
 
 //CVC
 inputCVC.addEventListener('input', function(){
-    const maxLength = 3,
-          updateCVC = this.value.padEnd(maxLength, defaultCVC);
-
+    const maxLength = 3;
     limitLength(this, maxLength);
-    checkValidations.checkCVC(this, maxLength);
+    const updateCVC = this.value.padEnd(maxLength, '0');
+
+    checkValidations.checkCVC(this, maxLength, validationCVC);
     detailCVC.textContent = updateCVC;
 
 });
